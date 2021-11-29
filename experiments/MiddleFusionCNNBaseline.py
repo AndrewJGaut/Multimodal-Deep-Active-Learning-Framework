056a022a54a3de49d579cb6ee53ae8b54108607a
Gordon Downs <gordiedowns@gmail.com>
2021-11-28 19:03:52 -0800

    move middle fusion model from notebook to scripts

---
+++ experiments/MiddleFusionCNNBaseline.py
@@ -0,0 +1,37 @@
+
+import sys
+sys.path.append("..")
+from models.multimodal.middle_fusion_model import MiddleFusionModel
+# from models.unimodal.squeezenet import SqueezeNet
+# from models.multimodal.combination_functions import MEAN_CLASSIFICATION
+from test_framework.tester import Tester
+import numpy as np
+from utils.data_utils import get_kaggle_satellite_image_classification_dataset_as_numpy_arrays
+from active_learning import categorical_query_functions as query_functions
+
+
+
+PATH_TO_DATA = "../data/kaggle_satellite_image_classification"
+active_learning_functions = [query_functions.RANDOM, query_functions.MIN_MAX, query_functions.MAX_ENTROPY, query_functions.MIN_MARGIN]
+active_learning_function_descriptions = ["Random", "Min-Max", "Max-Ent", "Min-Margin"]
+
+if __name__ == "__main__":
+    # get data
+    first_modality = get_kaggle_satellite_image_classification_dataset_as_numpy_arrays(PATH_TO_DATA)
+    second_modality = get_kaggle_satellite_image_classification_dataset_as_numpy_arrays(PATH_TO_DATA, True)
+    tester_x = np.stack((first_modality[0], second_modality[0]), axis=1)
+    tester_y = first_modality[1]
+
+    # define tester
+    tester = Tester(tester_x, tester_y, training_epochs=3, active_learning_loop_count=16)
+    tester.INITIAL_TRAIN_DATA_FRACTION = 0.05
+
+    for i,active_learning_function in enumerate(active_learning_functions):
+        # define model
+        multimodal_model = MiddleFusionModel(active_learning_function=active_learning_function)
+
+        # test model
+        tester.test_model(multimodal_model)
+
+    # get results
+    tester.plot_results()
---
+++ models/multimodal/middle_fusion_model.py
@@ -0,0 +1,275 @@
+import numpy as np
+from test_framework.model_interface import ModelInterface
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset, DataLoader
+import time
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train_model_given_numpy_arrays(model, x1, x2, y, criterion, optimizer, num_epochs=25, batch_size=8, is_inception=False, verbose=True):
+    if is_inception:
+        raise NotImplementedError("training using tensors not supported for inception model")
+
+    # Convert data to data loader so that it isn't allocated on the GPU all at once.
+    x1_tensor = torch.tensor(x1)
+    x2_tensor = torch.tensor(x2)
+    y_tensor = torch.tensor(y)
+    # y_tensor = torch.argmax(y_tensor, 1)
+    dataset = TensorDataset(x1_tensor,x2_tensor,y_tensor)
+    dataloader = DataLoader(dataset,batch_size=batch_size,num_workers=0,shuffle=True)
+
+    since = time.time()
+
+    for epoch in range(num_epochs):
+        # Each epoch has a training and validation phase
+        model.train()  # Set model to training mode
+
+        running_loss = 0.0
+        running_corrects = 0
+
+        for x1,x2, labels in dataloader:
+            x1 = x1.to(device)
+            x2 = x2.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(True):
+                # Get model outputs and calculate loss
+                outputs = model(x1,x2)
+                loss = criterion(outputs, labels)
+
+                _, preds = torch.max(outputs, 1)
+
+                # backward + optimize only if in training phase
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * x1.size(0)
+            running_corrects += torch.sum(preds == labels)
+
+        epoch_loss = running_loss / len(y)
+        epoch_acc = running_corrects.double() / len(y)
+        if verbose:
+            print('Epoch {}/{} - Train Loss: {:.4f} Acc: {:.4f}'.format(
+                epoch, num_epochs - 1, epoch_loss, epoch_acc))
+
+    if verbose:
+        print()
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return model
+
+
+
+'''
+Defines a wrapper for a middle fusion model based on AlexNet
+'''
+
+
+# https://pytorch.org/tutorials/beginner/examples_nn/two_layer_net_module.html
+# https://pytorch.org/tutorials/beginner/basics/buildmodel_tutorial.html
+# https://pytorch.org/vision/master/_modules/torchvision/models/alexnet.html
+
+class MiddleFusionNet(torch.nn.Module):
+    def __init__(self, num_classes: int = 4, dropout: float = 0.5) -> None:
+        super(MiddleFusionNet, self).__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(256 * 6 * 6 * 2, 4096), # added *2
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=dropout),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+    def forward(self, x_satellite: torch.Tensor, x_streetlevel: torch.Tensor) -> torch.Tensor:
+        # print(x.shape)
+        # x_satellite, x_streetlevel = x1, x2
+        #(x_satellite: torch.Tensor, x_streetlevel: torch.Tensor)
+        features_satellite = self.features(x_satellite)
+        features_satellite = self.avgpool(features_satellite)
+        features_satellite = torch.flatten(features_satellite, 1)
+
+        features_streetlevel = self.features(x_streetlevel)
+        features_streetlevel = self.avgpool(features_streetlevel)
+        features_streetlevel = torch.flatten(features_streetlevel, 1)
+        
+        all_features = torch.cat((features_satellite,features_streetlevel),1)
+
+        output = self.classifier(all_features)
+        return output
+    def details(self):
+        return "a middle fusion model based on AlexNet"
+
+class MiddleFusionModel(ModelInterface):
+    '''
+    Instantiate the middle fusion model.
+    '''
+    def __init__(self, active_learning_function=None,
+                 name=None, details=None,
+                num_epochs=3, batch_size=8, train_verbose=True,
+                query_function=None):
+        self.active_learning_function = active_learning_function
+        self.model = MiddleFusionNet(num_classes=4)
+        self._name = name
+        self._details = details
+
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model.to(device)
+        self._name = name
+        self._details = details
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.train_verbose = train_verbose
+        self.query_function = query_function
+
+        params_to_update = self.model.parameters()
+
+        # Observe that all parameters are being optimized
+        optimizer_ft = torch.optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+        self._optimizer = optimizer_ft
+
+        # store criterion
+        self._criterion = nn.CrossEntropyLoss()
+
+
+    # IDENTIFIER METHODS
+    '''
+    Unique name/ID for the model. This may be used by Tester to label plots and performance metrics.
+    Returns:
+        (str): Name for model
+    '''
+    def name(self) -> str:
+        if self._name is None:
+            return "Multimodal middle fusion model based on AlexNet"
+        return self._name
+
+    '''
+    Extra details about the model. May be used to specify hyperparameter values, etc which distinguish the
+    model from others but don't fit in the shorthand name.
+    '''
+    def details(self) -> str:
+        return self.model.details()
+
+    # # INTERACTION METHODS
+    # '''
+    # Function called to request that the model train on the given training dataset. Training should pass
+    # through the dataset only once (1 epoch). Testing framework will call repeatedly to achieve multiple epochs.
+    # Args:
+    #     train_x (np.ndarray):       Training inputs, with batch as the first axis.
+    #                                 In this case, these should have shape len(self.models), shape_of_data_for_each_modality
+    #                                 In other words, the index into axis0 gives the index of the modality
+    #     train_y (np.ndarray):       Training outputs for supervised learning, with batch as the first axis.
+    #                                 Should have shape (num_examples)
+    # Returns:
+    #     (float):    Average training loss
+    # '''
+    # def train(self, train_x: np.ndarray, train_y: np.ndarray) -> None:
+    #     swapped_axes_train_x = np.swapaxes(train_x, 0,1)
+    #     # print(train_x.shape)
+    #     train_x_satellite,train_x_streetlevel = swapped_axes_train_x
+    #     train_x_tuple = (train_x_satellite,train_x_streetlevel)
+    #     self.model.train(train_x_tuple,train_y)
+    #     # self.model.train((train_x_1,train_x_2),train_y)
+    #     # swapped_axes_train_x = np.swapaxes(train_x, 0,1) # this has shape num_modalities, shape_of_unimodal_data
+    #     # for i,model in enumerate(self.models):
+    #     #     model.train(swapped_axes_train_x[i], train_y)
+
+    # '''
+    # Function called to request that the model predict outputs for the given val/test dataset.
+    # Args:
+    #     test_x (np.ndarray):    Testing inputs, with batch as the first axis.
+    #                             Should have shape len(self.models),shape_of_each_modality
+    # Returns:
+    #     (np.ndarray):           Network outputs, with batch as the first axis, corresponding to each sample
+    #                             in test_x.
+    # '''
+
+    # def predict(self, test_x) -> np.ndarray:
+    #     #test_x_satellite: np.ndarray, test_x_streetlevel: np.ndarray
+    #     swapped_axes_test_x = np.swapaxes(test_x, 0,1)
+    #     test_x_satellite,test_x_streetlevel = swapped_axes_test_x
+    #     probabilities = self.model.predict_proba((test_x_satellite,test_x_streetlevel))
+    #     return np.argmax(probabilities,axis=1).reshape(-1,)
+
+    # '''
+    #  Function called to request that the model use its active learning algorithm to choose a subset of
+    #  'unlabeled' samples, which will then be labeled and added to the training set.
+    #  NOTE: The default implementation for this function samples randomly from the unlabeled set.
+    #  Args:
+    #      unlabeled_data (np.ndarray):    Batch of 'unlabeled' inputs from the dataset which are available to be
+    #                                      labeled and added to the training dataset in the next training iteration.
+    #      labeling_batch_size (int):      Number of 'unlabeled' inputs which should be chosen for labeling.
+    #  Returns:
+    #      (np.ndarray):   1-D array of indices into the <unlabeled_data> array, indicating which samples should be
+    #                      labeled and added to training dataset. Shape = (labeling_batch_size,).
+    #  '''
+
+    # def query(self, unlabeled_satellite_data: np.ndarray, unlabeled_streetlevel_data: np.ndarray, labeling_batch_size: int) -> np.ndarray:
+
+    #     if self.active_learning_function is None:
+    #         raise Exception("If you want to use the default multimodal query function, you need to supply an active learning function upon model instantiation.")
+
+    #     # default implementation is to use the active learning function provided as model input
+    #     # and call it on the mean of the output probabilities of the models.
+    #     preds = self.model.predict_proba(unlabeled_satellite_data,unlabeled_streetlevel_data)
+
+    #     return self.active_learning_function(preds, labeling_batch_size)
+
+
+
+    def train(self, train_x: np.ndarray, train_y: np.ndarray) -> None:
+        x1,x2 = np.swapaxes(train_x,0,1)
+        self.model = train_model_given_numpy_arrays(self.model, x1, x2, train_y, self._criterion, self._optimizer,
+                                                    self.num_epochs, self.batch_size, verbose=self.train_verbose)
+
+    def predict(self, test_x: np.ndarray):
+        self.model.eval()
+        x1,x2 = np.swapaxes(test_x,0,1)
+        x1 = torch.tensor(x1)
+        x2 = torch.tensor(x2)
+        dataset = TensorDataset(x1,x2)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, num_workers=0, shuffle=False)
+        preds_list = []
+        for (x1,x2,) in dataloader:
+            x1 = x1.to(device)
+            x2 = x2.to(device)
+            preds_list.append(self.model(x1,x2).cpu().detach().numpy())
+        return np.vstack(preds_list)
+
+    def predict_proba(self, test_x: np.ndarray) -> np.ndarray:
+        self.model.eval()
+        softmax = lambda x: np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+        softmax_outputs = softmax(self.predict(test_x))
+        return softmax_outputs
+
+    def query(self, unlabeled_data: np.ndarray, labeling_batch_size: int) -> np.ndarray:
+        if self.active_learning_function is None:
+            raise Exception("If you want to use the default multimodal query function, you need to supply an active learning function upon model instantiation.")
+        softmax_outputs = self.predict_proba(unlabeled_data)
+        indices = self.query_function(softmax_outputs, labeling_batch_size)
+        return indices
