8077062af79b266976fb881d00bb5d61d8cfaff8
Gordon Downs <gordiedowns@gmail.com>
2021-11-29 17:08:27 -0800

    make middle fusion CNN architecture way smaller. Much faster, and improves results, too.

--- models/multimodal/middle_fusion_model.py
+++ models/multimodal/middle_fusion_model.py
@@ -86,28 +86,14 @@ class MiddleFusionNet(torch.nn.Module):
             nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
         )
         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
             nn.Dropout(p=dropout),
-            nn.Linear(256 * 6 * 6, 4096),
+            nn.Linear(2304, 1024),
             nn.ReLU(inplace=True),
-            nn.Dropout(p=dropout),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            # nn.Linear(4096, num_classes),
         )
-        self.final_linear = nn.Linear(4096*2, num_classes)
+        self.final_linear = nn.Linear(1024*2, num_classes)
     def forward(self, x1: torch.Tensor, x2: torch.Tensor, x2_image_counts: torch.Tensor) -> torch.Tensor:
         x1_features = self.features(x1)
         x1_features = self.avgpool(x1_features)
@@ -123,7 +109,7 @@ class MiddleFusionNet(torch.nn.Module):
                 cur_x2_embeddings = self.classifier(cur_x2_features)
                 x2_embeddings.append(torch.mean(cur_x2_embeddings,axis=0))
             else:
-                x2_embeddings.append(torch.zeros(4096).to(device))
+                x2_embeddings.append(torch.zeros(1024).to(device))
         x2_embeddings = torch.stack(x2_embeddings)
 
         all_embeddings = torch.cat((x1_embeddings,x2_embeddings),1)
